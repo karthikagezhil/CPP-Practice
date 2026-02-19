851856cba1034b44ea3934a9e204052a2a93ff21
Ezhil Vadivel Thanarasu <karthikagezhil@gmail.com>
2026-02-19 12:05:26 -0600

    auto return type

---
+++ auto-return.cpp
@@ -0,0 +1,15 @@
+// Use of auto? 
+
+#include <iostream>
+using namespace std;
+
+template <typename T1, typename T2>
+auto add(T1 a, T2 b) {
+    return a+b;
+}
+
+int main() {
+    cout << add(1, 1.8) << endl;
+    cout << add(1.8, 1);
+    return 0;
+}
